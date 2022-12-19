ae10dd1d2609a41c79b7b9d3301a939b73e5d8b3
Roma-Leto <119625441+roma-leto@users.noreply.github.com>
2022-12-19 06:43:57 +0300

    Initial commit

---
+++ testchannal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TestchannalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'testchannal'
---
+++ testchannal/consumers.py
@@ -0,0 +1,12 @@
+from channels.generic.websocket import WebsocketConsumer
+import json
+from time import sleep
+from random import randint
+
+class WSConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+        for i in range(1000):
+           self.send(json.dumps({'message': randint(1, 100)}))
+           sleep(1)
